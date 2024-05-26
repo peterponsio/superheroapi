369ce0e0dc89e4cb4c01d5f16c4b9ad790548fd2
Pedro Perera <pedrodeveloperera@gmail.com>
2024-05-26 20:44:33 +0200

    fix port flask api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS # type: ignore
+import os
 
 app = Flask(__name__)
 CORS(app)  
@@ -125,4 +126,5 @@ def delete_superheroe(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=33507)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
