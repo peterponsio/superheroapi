986913147623cdb2220cafa7562ba8eacd036951
Pedro Perera <pedrodeveloperera@gmail.com>
2024-05-26 20:21:00 +0200

    fix port flask

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS # type: ignore
-
+import os 
 
 app = Flask(__name__)
 CORS(app)  
@@ -124,5 +124,7 @@ def delete_superheroe(id):
     superheroes = [heroe for heroe in superheroes if heroe["id"] != id]
     return ('', 204)
 
+
+port = int(os.environ.get('PORT', 33507))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=33507)
