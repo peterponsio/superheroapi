17edaa191b9e60d758839bf14f9e030314f3a92c
Pedro Perera <59996621+peterponsio@users.noreply.github.com>
2024-05-27 10:00:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS, cross_origin # type: ignore
 import os
 
 app = Flask(__name__)
-CORS(app, support_credentials=True))  
+CORS(app, support_credentials=True)  
 
 superheroes = [
   {
